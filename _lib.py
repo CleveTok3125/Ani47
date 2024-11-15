f8e28c77b8fa892b0fc28b92c50e7088beff146a
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2024-11-15 13:54:13 +0700

    Add files via upload

---
+++ _lib.py
@@ -0,0 +1,169 @@
+from urllib.parse import urlparse
+from urllib.parse import urljoin
+import requests, re, ast, os, sys
+
+try:
+	import webview
+	wv_supported = True
+except:
+	wv_supported = False
+
+def get_source(url):
+	r = requests.get(url)
+	return r.content.decode('utf-8')
+
+def fetch(host, js_code):
+	anime_name = None
+	match = re.search(r'<title>(.*?)</title>', js_code)
+	if match:
+		anime_name = match.group(1)
+
+	macdinh = re.search(r'var macdinh = (\d+);', js_code)
+	sv_phu = re.search(r'var sv_phu = (\d+);', js_code)
+	id_ep = re.search(r'var id_ep = (\d+);', js_code)
+
+	data = {
+		'ID': int(id_ep.group(1)) if id_ep else None,
+		'SV': int(macdinh.group(1)) if macdinh else None,
+		'SV4': int(sv_phu.group(1)) if sv_phu else None
+	}
+
+	response = requests.post(f'https://{host}/player/player.php', data=data)
+
+	if response.status_code == 200:
+		match = re.search(r'https?://.*?\.m3u8', response.text)
+		if match:
+			video_url = match.group(0)
+			return (anime_name, video_url)
+		else:
+			print("This is a warning, not an error when running the program: Video format not supported.")
+			return False
+	else:
+		print("Request failed:", response.status_code)
+		return False
+
+def player(anime_name, video_url, hsize, wsize):
+	html_file = os.path.normpath('./player.html')
+	temp_html = os.path.normpath('./temp.player.html')
+
+	if os.path.exists(temp_html):
+		os.remove(temp_html)
+
+	with open(html_file, 'r', encoding='utf-8') as file:
+		html_content = file.read()
+	html_content = html_content.replace("{{video_url}}", video_url)
+	if not wv_supported:
+		html_content = html_content.replace('controlsList="nofullscreen"', '')
+		html_content = html_content.replace('var isFeatureEnabled = true', 'var isFeatureEnabled = false')
+		
+	with open(temp_html, 'w', encoding='utf-8') as file:
+		file.write(html_content)
+
+	if wv_supported:
+		window = webview.create_window(anime_name, temp_html, width=wsize, height=hsize, resizable=True, easy_drag=True)
+		webview.start()
+	else:
+		print(f'Pywebview is not supported. Access player via "{os.path.join(os.getcwd(), temp_html)}"')
+		input('Press Enter to end session\n')
+	os.remove(temp_html)
+
+def search(host, query):
+	def search_by_query(query):
+		for title, href in movie_dict.items():
+			if query.lower() in title.lower():
+				return {title: href}
+		return None
+
+	pattern = r'<a\s+class="movie-item\s+m-block"[^>]*\s+title="([^"]+)"\s+href="([^"]+)"[^>]*>'
+	url = f'https://{host}/tim-nang-cao/?keyword={query}'
+	source = get_source(url)
+	matches = re.findall(pattern, source)
+	movie_dict = {match[0]: match[1] for match in matches}
+	result = search_by_query(query)
+	if result:
+		for title, href in result.items():
+			return title, href
+	else:
+		return False
+
+def eps(host, path):
+	base = f'https://{host}/'
+	url = urljoin(base, path)
+	sauce = get_source(url)
+	pattern = r"episodePlay\s*=\s*'([^']+)'"
+	match = re.search(pattern, sauce)
+	if match:
+		ep1 = match.group(1)
+	else:
+		print("Episode not found.")
+		return False
+	url = urljoin(base, ep1)
+	sauce = get_source(url)
+	pattern = r'''<a[^>]*\s+href="([^"]+)"[^>]*(?=\s+class="[^"]*(?:active\s+)?btn-episode[^"]*")[^>]*>(.*?)<\/a>'''
+	episodes = {}
+	matches = re.findall(pattern, sauce)
+	for href, episode in matches:
+		episodes[episode] = urljoin(base, href)
+	return episodes
+
+def clscr():
+	if sys.platform == "win32":
+		os.system("cls")
+	else:
+		os.system("clear")
+
+def menu(items, ask=None):
+	items = list(items)
+	length = len(items)
+	items = [f"{index}. {item}" for index, item in enumerate(items, start=1)]
+	while True:
+		clscr()
+		if ask != None:
+			print(ask)
+		print('\n'.join(items))
+		try:
+			ans = int(input('Select one: '))
+			if ans > 0 and ans <= length:
+				return ans-1
+			else:
+				input('Invalid choice')
+		except ValueError:
+			input('Invalid input. Please enter a number')
+
+def menudict(items, ask=None, presel=False):
+	items = list(items.items())
+	length = len(items)
+	while True:
+		clscr()
+		if ask != None:
+			print(ask)
+		if presel == False:
+			for i in range(length):
+				key, value = items[i]
+				print(f'{i+1}. Ep {key}')
+		try:
+			ans = int(input('Select one: ')) if presel == False else presel
+			if 1 <= ans <= length:
+				key, value = items[ans - 1]
+				return value, ans, length
+			else:
+				input('Invalid choice')
+		except ValueError:
+			input('Invalid input. Please enter a number')
+
+def check_connection(url):
+	print('Checking connection...')
+	try:
+		response = requests.get(url, timeout=5)
+		if response.status_code == 200:
+			print("Connection successful!")
+			return True
+		else:
+			print(f"Error: Status code {response.status_code}")
+	except requests.ConnectionError:
+		print("Unable to connect to the network.")
+	except requests.Timeout:
+		print("Connection timed out.")
+	except requests.RequestException as e:
+		print(f"An error occurred: {e}")
+	return False
---
+++ main.py
@@ -0,0 +1,91 @@
+from _lib import *
+from urllib.parse import urljoin
+import time, os
+
+class AnimePlayer:
+	def __init__(self, host, hsize, wsize):
+		self.host = host
+		self.hsize = hsize
+		self.wsize = wsize
+		self.ep_selected = 1
+		self.total_ep = 0
+		self.title = ''
+		self.ep_list = []
+
+	def search_anime(self):
+		query = str(input('Search Anime: '))
+		result = search(self.host, query)
+		if result != False:
+			self.title, href = result
+			self.ep_list = eps(self.host, href)
+			self.url, self.ep_selected, self.total_ep = menudict(ask=self.title, items=self.ep_list)
+			self.url = urljoin(f'https://{self.host}/', self.url)
+			js_code = get_source(self.url)
+			anime_name, video_url = fetch(self.host, js_code)
+			clscr()
+			print('''Video player gesture instructions:
+- Play/Pause: double click
+- Backward 10s: click left side
+- Forward 10s: click right side
+- Fast forward x2: Hold (1sec) right side
+- Forward 85s (skip OP/EN): click top side
+- Move window: drag down side''')
+			player(anime_name, video_url, self.hsize, self.wsize)
+			self.show_actions_menu()
+
+	def show_actions_menu(self):
+		opts = menu(ask=f'{self.title}\nCurrent Episode: {self.ep_selected}', items=['Previous Episode', 'Next Episode', 'Search Anime', 'Exit'])
+		if opts == 0:  # Previous
+			self.previous_episode()
+		elif opts == 1:  # Next
+			self.next_episode()
+		elif opts == 2:  # Search
+			self.search_anime()
+		elif opts == 3:
+			os._exit(0)
+		else:
+			self.show_actions_menu()
+
+	def previous_episode(self):
+		if self.ep_selected == 1:
+			input('No previous episode')
+			self.show_actions_menu()
+		else:
+			self.ep_selected -= 1
+			self.url, self.ep_selected, self.total_ep = menudict(ask=None, items=self.ep_list, presel=self.ep_selected)
+			self.url = urljoin(f'https://{self.host}/', self.url)
+			js_code = get_source(self.url)
+			anime_name, video_url = fetch(self.host, js_code)
+			clscr()
+			player(anime_name, video_url, self.hsize, self.wsize)
+			self.show_actions_menu()
+
+	def next_episode(self):
+		if self.ep_selected == self.total_ep:
+			input('No next episode')
+			self.show_actions_menu()
+		else:
+			self.ep_selected += 1
+			self.url, self.ep_selected, self.total_ep = menudict(ask=None, items=self.ep_list, presel=self.ep_selected)
+			self.url = urljoin(f'https://{self.host}/', self.url)
+			js_code = get_source(self.url)
+			anime_name, video_url = fetch(self.host, js_code)
+			clscr()
+			player(anime_name, video_url, self.hsize, self.wsize)
+			self.show_actions_menu()
+
+def main():
+	host = 'anime47.cam'
+	hsize, wsize = (545, 900)
+
+	if check_connection(f'https://{host}/'):
+		time.sleep(0.5)
+		clscr()
+	else:
+		return check_connection
+
+	anime_player = AnimePlayer(host, hsize, wsize)
+	anime_player.search_anime()
+
+if __name__ == '__main__':
+	main()
