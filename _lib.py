a4595322551bdc5314574be39953c34337c57145
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2024-12-15 03:02:46 +0700

    Add option to enable/disable local sharing

--- _lib.py
+++ _lib.py
@@ -124,10 +124,11 @@ def player(anime_name, video_url, track_lst, hsize, wsize):
 	else:
 		from server import start_server
 		PORT = _config.get_int('PORT')
-		print(f'''Pywebview is not supported.\nAccess player via "{urljoin(f'http://127.0.0.1:{PORT}/', temp_html)}"''')
+		share = _config.get_bool('LOCAL_SHARE')
+		print(f'''Pywebview is not supported.\nAccess player via "{urljoin(f'http://127.0.0.1:{PORT}/', temp_html)}"{" or IP address" if share else ""}''')
 		print('Local server is running... (CTRL-C to stop)')
 		try:
-			start_server(PORT)
+			start_server(PORT=PORT, share=share)
 		except KeyboardInterrupt:
 			pass
 	
