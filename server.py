7999ce79466c5c4e96360c2cde7e55818d8c1525
CleveTok3125 <97098535+CleveTok3125@users.noreply.github.com>
2024-12-15 03:02:46 +0700

    Add option to enable/disable local sharing

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 import http.server
 import socketserver
 
-def start_server(PORT=8000):
-	with socketserver.TCPServer(("", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
+def start_server(PORT=8000, share=False):
+	with socketserver.TCPServer(("" if share else "127.0.0.1", PORT), http.server.SimpleHTTPRequestHandler) as httpd:
 		httpd.serve_forever()
